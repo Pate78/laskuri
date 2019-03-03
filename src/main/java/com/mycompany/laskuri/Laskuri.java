56f10eba11c0e9c9407f321acd20c29b4259a0c4
patri <patri@PATELAPTOP>
2019-03-03 17:05:50 +0200

    commit 2

--- src/main/java/com/mycompany/laskuri/Laskuri.java
+++ src/main/java/com/mycompany/laskuri/Laskuri.java
@@ -23,9 +23,10 @@ public class Laskuri{
 	public Laskuri(){
 		this.tapahtumia = 0;
 	}
+	
 
-  	public void lisaa(){
-    		this.tapahtumia = this.tapahtumia + 1; 
+  	public void lisaa(int lisays){
+    		this.tapahtumia = this.tapahtumia + lisays; 
   	}
   	public int lukema(){
     		return this.tapahtumia;
@@ -34,7 +35,7 @@ public class Laskuri{
     		this.tapahtumia = 0;
   	}
 	public String toString(){
-		return "Laskurin lukema on " + this.tapahtumia;
+		return "Lukema:" + this.tapahtumia;
 	}
 
 }
